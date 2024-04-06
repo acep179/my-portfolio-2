562a9b43498fac1131889097431d49e2fb48db47
Acep Awaludin <acep.awaludin179@gmail.com>
2024-04-07 04:45:14 +0700

    [FEAT : FLOATBTN] - Add a tooltip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import {
   Box,
   useColorMode,
   IconButton,
+  Tooltip
 } from "@chakra-ui/react";
 import { Navbar, Header, FloatButtonContainer } from "../templates";
 
@@ -20,17 +21,19 @@ const IndexPage: FC<PageProps> = () => {
       <Header />
       
       <FloatButtonContainer>
-        <IconButton
-          backgroundColor={colorMode === 'light' ? 'white' : 'blackalpha.50' }
-          position='relative'
-          aria-label="Toggle Color Mode"
-          variant='outline'
-          onClick={toggleColorMode}
-          colorScheme="teal"
-          fontSize='20px'
-          isRound={true}
-          icon={colorMode === 'light' ? <FaMoon /> : <FaSun /> }
-        />
+        <Tooltip label={`Change theme to ${colorMode === 'light' ? 'dark' : 'light' } mode`} closeOnClick={false}  placement='top' hasArrow >
+          <IconButton
+            backgroundColor={colorMode === 'light' ? 'white' : 'blackalpha.50' }
+            position='relative'
+            aria-label="Toggle Color Mode"
+            variant='outline'
+            onClick={toggleColorMode}
+            colorScheme="teal"
+            fontSize='20px'
+            isRound={true}
+            icon={colorMode === 'light' ? <FaMoon /> : <FaSun /> }
+          />
+        </Tooltip>
       </FloatButtonContainer>
     </Box>
   );

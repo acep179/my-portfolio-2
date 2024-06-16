1ad9fcd8c30fe69efbc96f420f6f4fbe34f95555
Acep Awaludin <acep.awaludin179@gmail.com>
2024-06-16 14:13:36 +0700

    [FEAT : ABOUT] - Add My Photo to About Section

--- src/templates/About.tsx
+++ src/templates/About.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
 import {
   AbsoluteCenter,
+  Flex,
   Grid,
   GridItem,
+  Image,
   Text,
 } from "@chakra-ui/react";
 import { SectionHeading } from "../components";
@@ -19,21 +21,23 @@ const About = () => {
       templateColumns={['repeat(1, 1fr)', null, 'repeat(5, 1fr)']}
       gap={6}
     >
-      <SectionHeading title="1. About" colSpan={[0, null, 5]} />
+      <SectionHeading title="1. About" placeContent='end' colSpan={[0, null, 3]} />
 
-      <GridItem position='relative' order={[2, null, 1]} w='full' h={300} colSpan={[0, null, 3]} >
-        <AbsoluteCenter axis='vertical'>
+      <GridItem order={[2, null, 2]} w='full' colSpan={[0, null, 3]} >
+        <Flex flexDirection='column' height='100%'>
           <Text mb={3}>
             Hello! My name is Acep Awaludin. I am a Fullstack Web Developer who works at PT. Integrasi Logistik Cipta Solusi (ILCS), Jakarta. I graduated from UIN Jakarta and an alumnus of Bootcamp Dumbways batch 37.
           </Text>
           <Text>
             I have skills in React.js, Next.js, Tailwind CSS, and Bootstrap on the frontend. On the backend, I have skills in Node.js-Express.js or Hapi.js. I also have experience in using Golang-Gorilla/mux for some projects.
           </Text>
-        </AbsoluteCenter>
+        </Flex>
       </GridItem>
 
-      <GridItem borderRadius={20} order={[1, null, 2]} w='full' h={300} colSpan={[0, null, 2]} borderWidth={3} borderColor='teal'>
-
+      <GridItem borderRadius={20} order={[1, null, 1]} w='full' rowSpan={[0, null, 2]} colSpan={[0, null, 2]} borderWidth={[3, 4, 4]} borderColor='teal' overflow='hidden'>
+        <Image 
+          src='/img/acep.jpg'
+          alt='Acep Awaludin'  />
       </GridItem>
     </Grid>
   )
